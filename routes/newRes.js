c8471c98a3a709058ba33233bb28ad765d885e78
Olivia <wangjueolivia@gmail.com>
2017-05-02 17:46:12 -0700

    created  new restaurant recommend page

---
+++ routes/newRes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+module.exports = function () {
+   router.get('/', function(req, res) {
+       console.log("in example");
+       res.render('pages/example'); // normally, we ignore the extension name of example.html file
+   });
+
+   return router;
+};
