e55dababa948da7860630c6b5a1316284005430c
Olivia <wangjueolivia@gmail.com>
2017-05-11 20:11:05 -0700

    newRest page updated

--- routes/newRes.js
+++ routes/newRes.js
@@ -3,7 +3,14 @@ var router = express.Router();
 module.exports = function (firebase) {
    router.get('/', function(req, res) {
        console.log("new openings");
-       res.render('pages/newRes'); // normally, we ignore the extension name of example.html file
+        var list=[{name:"a",category:"hotpot",address:"1 street"},
+         {name:"b",category:"dimsum",address:"2 street"},{name:"c",category:"noodle"}];
+       res.render('pages/newRes',{temp:list});
+
+        // normally, we ignore the extension name of example.html file
+
+
+
    });
 
    return router;
