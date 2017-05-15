ed9f01b0700f3362e5be953ade121889c7b99807
GMX\Mingxiao Guo <marshallguo0221@gmail.com>
2017-05-15 00:10:36 -0700

    update searchResult and footer

--- routes/searchResult.js
+++ routes/searchResult.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (firebase) {
-  // router.get('/', function(req, res) {
-  //   console.log("in all restaurant");
-  //   firebase.database().ref("/restaurant/").once('value').then(function(snapshot) {
-  //     var temp = snapshot.val();
-  //     console.log(temp);
-  //     var arr = [];
-  //     Object.keys(temp).forEach(function (key, index) {
-  //       console.log(temp[key].name);
-  //       arr[index] = {};
-  //       arr[index]=temp[key];
-  //       arr[index].id = key;
-  //       console.log("category is :",temp[key].category);
-  //     });
-  //     console.log(arr);
-  //     console.log("search result page");
-  //     res.render('pages/searchResult',{searchResult:arr});
-  //   });
-  // });
+  router.get('/', function(req, res) {
+    console.log("in all restaurant");
+    firebase.database().ref("/restaurant/").once('value').then(function(snapshot) {
+      var temp = snapshot.val();
+      console.log(temp);
+      var arr = [];
+      Object.keys(temp).forEach(function (key, index) {
+        console.log(temp[key].name);
+        arr[index] = {};
+        arr[index]=temp[key];
+        arr[index].id = key;
+        console.log("category is :",temp[key].category);
+      });
+      console.log(arr);
+      console.log("search result page");
+      res.render('pages/searchResult',{searchResult:arr});
+    });
+  });
 
   router.get("/:content", function (req, res) {
     console.log("in content");
